1d65884c17637da6a2df6637a677048e6d931d45
KeithBoehler <germanbohler@gmail.com>
2022-01-19 14:50:42 -0600

    Changed the main program to work with the obj ort version to be more org

--- main.py
+++ main.py
@@ -1,35 +1,11 @@
 import numpy as np
-from exif import Image
 
 
-print("hello")
-img_dir = "/mnt/c/Users/AS/Pictures/America Ship 3D/"
-img_path1 = img_dir + "R0012338.JPG"
+from METADAT3D import THREE_D_META
 
 
+x = THREE_D_META("/mnt/c/Users/AS/Pictures/America Ship 3D/", "R0012343.JPG")
 
-with open(img_path1, 'rb') as image_file:
-    my_image = Image(image_file)
-    
-print(img_path1)
-print(my_image.gps_latitude)
-print(my_image.gps_longitude)
-
-
-
-img_path1 = img_dir + "R0012343.JPG"
-with open(img_path1, 'rb') as image_file:
-    my_image2 = Image(image_file)
-    
-print(img_path1)
-print(my_image2.gps_latitude)
-print(my_image2.gps_longitude)
-
-
-img_path1 = img_dir + "R0012369.JPG"
-with open(img_path1, 'rb') as image_file:
-    my_image3 = Image(image_file)
-    
-print(img_path1)
-print(my_image3.gps_latitude)
-print(my_image3.gps_longitude)
+x.getImageName()
+x.getImageDir()
+x.getHeader()
