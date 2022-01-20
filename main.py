c880b11d61976b381d46ccc20ff40fb883df7a57
KeithBoehler <germanbohler@gmail.com>
2022-01-20 11:44:18 -0600

    Arranged func to load 3d pics into mem

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-import numpy as np
-
-
+import os
 from METADAT3D import THREE_D_META
 
 
-x = THREE_D_META("/mnt/c/Users/AS/Pictures/America Ship 3D/", "R0012343.JPG")
+def loadPicsList(dir):    
+    """ Get the pics from disk to mem
+    :param dir: The parth to the folder with the 3d images of interest.     
+    """ 
+    pics_list = os.listdir(dir)
+    for i in range(len(pics_list)):
+        pic = THREE_D_META(dir, pics_list[i])
+        pics_list[i] = pic
+    return pics_list
 
-x.getImageName()
-x.getImageDir()
-x.getHeader()
+   
+pics_dir = "/mnt/c/Users/AS/Pictures/America Ship 3D/"
+
+pics = loadPicsList(pics_dir)
+print(pics)
+
+
