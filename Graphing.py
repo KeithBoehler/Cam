a34da133554b1ce4dcc20138820370d46d35f622
KeithBoehler <germanbohler@gmail.com>
2022-03-02 14:58:06 -0600

    The glue for the nodes

---
+++ Graphing.py
@@ -0,0 +1,23 @@
+'''
+Keith Boehler, 2022 March 1
+
+Objective: Create Directed Graphs to simulate 3d tour
+
+PreCond:
+
+PostCond:
+
+Bugs:
+
+'''
+from METADAT3D import THREE_D_META
+
+
+class graphing:
+    active_node = "empty"
+    
+    def __init__(self, three_dim_pic_meta):
+    
+
+
+
