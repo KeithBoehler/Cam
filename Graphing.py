de67fb2bcc75f60c5bd9f24907aab5602506eebc
KeithBoehler <germanbohler@gmail.com>
2022-03-03 11:28:30 -0600

    added moveLocation().

--- Graphing.py
+++ Graphing.py
@@ -13,17 +13,17 @@ Bugs:
 from METADAT3D import THREE_D_META
 
 
-class graphing:
+class Graphing:
     __tour_guide_location = "empty"
     __nodes_dict = dict()
     
     def __init__(self, tour_spots, tour_enterance = "none"):
-        self.tour_spots_updater(tour_spots)
+        self.tourSpotsUpdater(tour_spots)
         self.__tour_guide_location = tour_enterance
     
     
-    def tour_spots_updater(self, new_spot):
-    """:param new_spot: New spot for the tour. Will be a dicionary with name of photo as key
+    def tourSpotsUpdater(self, new_spot):
+        """:param new_spot: New spot for the tour. Will be a dicionary with name of photo as key
         and the value of the obj address (node)
         
         precond: image 3d data has been loaded into node
@@ -35,14 +35,17 @@ class graphing:
         """
         self.__nodes_dict.update(new_spot)
 
-    def move_location(self):
+    def moveLocation(self):
         possible_moves = self.__tour_guide_location.getNeigborhood()
         i = 0
         tmp = dict()
         for place in possible_moves:
             print(i, " : ", place)
-            tmp.update({i : place})
+            tmp.update({i : possible_moves[place]})
             i = i + 1
         user = input("Select number for next room:  ")
-        __tour_guide_location = tmp[user]
+        user = int(user)
+        self.__tour_guide_location = self.__nodes_dict[tmp[user].getImageName()]
 
+    def getTourLocation(self):
+        return self.__tour_guide_location.whoAmI()
